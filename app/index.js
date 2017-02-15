ff2446bdf705e5673dea2e03575aec7566827cd2
Fahad Qazi <fahadqazi@gmail.com>
2017-02-15 23:27:52 +0000

    working with props and components

--- app/index.js
+++ app/index.js
@@ -1,12 +1,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+    name: 'Fahad Qazi',
+    username: 'fahadqazi',
+    image: 'https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg'
+}
+
+var ProfilePic = React.createClass({
     render: function(){
-        return(
-            <div>Hello React World!</div>
+        return (
+            <img src={this.props.imageUrl} style={{height:100, width:100}} />
         );
     }
-});
+})
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+var ProfileLink = React.createClass({
+    render: function(){
+        return(
+            <div className="ProfileLink">
+                <a href={'https://www.github.com/' + this.props.username} ></a>
+                {this.props.username}
+            </div>
+        );
+    }
+})
+
+var ProfileNmae = React.createClass({
+    render: function(){
+        return(
+            <div className="name">
+                {this.props.name}
+            </div>
+        );
+    }
+})
+
+var Avatar = React.createClass({
+    render: function(){
+        return(
+            <div className="Avatar">
+                <ProfilePic imageUrl={this.props.user.image}/>
+                <ProfileNmae name={this.props.user.name}/>
+                <ProfileLink username={this.props.user.username}/>
+            </div>
+        );
+    }
+})
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
